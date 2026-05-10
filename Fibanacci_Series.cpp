1e564b5802d113240f3329668d4c9d76a2111ed3
Ahmed Said <ahmed.said.gad1@gmail.com>
2026-05-10 18:23:17 +0300

    Add Fibonacci Series functionality

---
+++ Fibanacci_Series.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printFibonacci(int n)
+{
+	int a = 0, b = 1, nextTerm;
+	cout << "Fibonacci Series: ";
+	for (int i = 1; i <= n; ++i) {
+		cout << a << " ";
+		nextTerm = a + b;
+		a = b;
+		b = nextTerm;
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the number of terms: ";
+	cin >> n;
+	printFibonacci(n);
+	return 0;
+}
