c2e90d08444cd0828234f831f42b3c52bcaeab90
Ahmed Said <ahmed.said.gad1@gmail.com>
2026-05-10 19:16:02 +0300

    Add functionality to print the first letter of each word in a string

---
+++ First_Letter_In_String2.cpp
@@ -0,0 +1,29 @@
+// program to print the first letter in each word of a string
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Read_string()
+{
+	string str;
+	cout << "Enter a string: ";
+	getline(cin, str);
+	return str;
+}
+
+void Print_first_letter(string str)
+{
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (i == 0 || str[i - 1] == ' ')
+		{
+			cout << char(toupper(str[i])) << " ";
+		}
+	}
+}
+
+ int main()
+{	string str = Read_string();
+	Print_first_letter(str);
+	return 0;
+}
